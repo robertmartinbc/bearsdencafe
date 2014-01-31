086d050d5f0f32f0de6f1ff9fba65eb4698fc975
Robert Martin <robertmartinbc@shaw.ca>
2014-01-30 22:13:28 -0800

    First commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bearsdencafe::Application.config.secret_token = 'b4e917152125061ba563a70a381e43de060f3eae0e52b2387662457dc088bd282162ec9689d2b0573448d6ad1cf5ce0dae0588ff30de8b4e86e017fec7f12ea6'
